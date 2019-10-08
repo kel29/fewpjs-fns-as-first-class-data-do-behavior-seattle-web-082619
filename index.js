7e57e210d1ec361d35321625cb576f6531b7fc2b
Krista Loven <krista.loven.87@gmail.com>
2019-10-08 11:55:10 -0700

    Done.

--- index.js
+++ index.js
@@ -7,5 +7,19 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function greet(time) {
+  const times = time.split(':');
+  const hour = parseInt(times[0], 10);
+  if (hour < 12) {
+    return "Good Morning"
+  } else if (hour < 17) {
+    return "Good Afternoon"
+  } else if (hour < 24) {
+    return "Good Evening"
+  }
+}
+
+function displayMessage(string="Not a valid time") {
+  let greeting = document.getElementById('greeting');
+  greeting.innerText = string;
+} 
